6c29cb4689521123d32e8d90ff032f04871dee1b
Thiago Lino <thiagolinu@gmail.com>
2023-11-27 11:53:11 -0300

    populando os dados

--- src/api/game/controllers/game.ts
+++ src/api/game/controllers/game.ts
@@ -1,18 +1,18 @@
-/**
- * game controller
- */
-
 import { factories } from "@strapi/strapi";
 
 export default factories.createCoreController(
   "api::game.game",
   ({ strapi }) => ({
-    async populate(ctx: any) {
-      console.log("RODANDO NO SERVIDOR");
+    async populate(ctx) {
+      const options = {
+        limit: 48,
+        order: "desc:trending",
+        ...ctx.query,
+      };
 
-      await strapi.service("api::game.game").populate(ctx.query);
+      await strapi.service("api::game.game").populate(options);
 
-      ctx.send("FINALIZANDO NO CLIENTE");
+      ctx.send("Finished populating games!");
     },
   })
 );
