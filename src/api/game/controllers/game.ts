380c5ff40e31071b266f240e8dd43fbf2ea456ab
Thiago Lino <thiagolinu@gmail.com>
2023-11-27 10:11:52 -0300

    Get data with JSDOM scrapper

--- src/api/game/controllers/game.ts
+++ src/api/game/controllers/game.ts
@@ -2,6 +2,17 @@
  * game controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController('api::game.game');
+export default factories.createCoreController(
+  "api::game.game",
+  ({ strapi }) => ({
+    async populate(ctx: any) {
+      console.log("RODANDO NO SERVIDOR");
+
+      await strapi.service("api::game.game").populate(ctx.query);
+
+      ctx.send("FINALIZANDO NO CLIENTE");
+    },
+  })
+);
